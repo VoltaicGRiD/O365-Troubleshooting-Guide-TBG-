35e33d2eba319d4067a39c649bd6477426dc7430
Dustin Conlon <58367364+VoltaicGRiD@users.noreply.github.com>
2021-04-20 14:22:56 -0600

    Redacted Azure Storage Account Key

--- Blob_Controller.cs
+++ Blob_Controller.cs
@@ -8,6 +8,6 @@ namespace TBG_WPF
 {
     public class Blob_Controller
     {
-        public const string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=screenshotstorage;AccountKey=gsVEa/QofWSkgWmgKmM+HMhzdp/Jhmi0AFcZcDBXea1uNHHuKmJpbcdxez3mxIgyKw6t1CMisKmpSKXd2An53w==;EndpointSuffix=core.windows.net";
+        public const string ConnectionString = "REDACTED";
     }
 }
